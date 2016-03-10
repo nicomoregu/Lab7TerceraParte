8fbeeb5f1569e23d9a871fb72c281708b0737431
nicomoregu <nicomoregu@gmail.com>
2016-03-09 22:54:16 -0500

    Vista registroconsultas

--- src/main/java/edu/eci/pdsw/samples/managedbeans/RegistroConsultaBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/RegistroConsultaBean.java
@@ -16,8 +16,16 @@
  */
 package edu.eci.pdsw.samples.managedbeans;
 
+import edu.eci.pdsw.samples.entities.Consulta;
+import edu.eci.pdsw.samples.entities.Paciente;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosPacientes;
 import edu.eci.pdsw.samples.services.ServiciosPacientes;
 import java.io.Serializable;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Logger;
 import javax.annotation.ManagedBean;
 import javax.enterprise.context.SessionScoped;
 
@@ -29,7 +37,112 @@ import javax.enterprise.context.SessionScoped;
 @SessionScoped
 public class RegistroConsultaBean implements Serializable{
     
-    ServiciosPacientes sp=ServiciosPacientes.getInstance();
+    private ServiciosPacientes sp=ServiciosPacientes.getInstance();
+    private Paciente pacienteConsulta = new Paciente(123, "CC", "Juan Perez", java.sql.Date.valueOf("2000-01-01"));
+    private String nombrePaciente="";
+    private int id=0;
+    private String tipoId="";
+    private Date fechaPaciente=null;
+    private String resumenConsul="";
+    private List<Consulta> consultasPaciente = new ArrayList<>();
+    private Date fechaConsul=null;
     
+    public void agregarConsulta() throws ExcepcionServiciosPacientes{
+        try{
+            sp.agregarConsultaAPaciente(id, tipoId, new Consulta(fechaConsul, resumenConsul));
+            System.out.println("agrego consulta");
+        }catch (EnumConstantNotPresentException e){
+            
+        }
+        this.getConsultasPaciente();
+    }
+    
+    public Date getFechaConsul() {
+        return fechaConsul;
+    }
+
+    public void setFechaConsul(Date fechaConsul) {
+        this.fechaConsul = fechaConsul;
+    }
+
+    public List<Consulta> getConsultasPaciente() {
+        Iterator iterador = pacienteConsulta.getConsultas().iterator();
+        consultasPaciente.clear();
+        while(iterador.hasNext()){
+            consultasPaciente.add((Consulta)iterador.next());
+        }
+        return consultasPaciente;
+    }
+
+    public void setConsultasPaciente(List<Consulta> consultasPaciente) {
+        this.consultasPaciente = consultasPaciente;
+    }
+
+    public ServiciosPacientes getSp() {
+        return sp;
+    }
+
+    public void setSp(ServiciosPacientes sp) {
+        this.sp = sp;
+    }
+
+    public String getNombrePaciente() {
+        return nombrePaciente;
+    }
+
+    public void setNombrePaciente(String nombrePaciente) {
+        this.nombrePaciente = nombrePaciente;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipoId() {
+        return tipoId;
+    }
+
+    public void setTipoId(String tipoId) {
+        this.tipoId = tipoId;
+    }
+
+    public Date getFechaPaciente() {
+        return fechaPaciente;
+    }
+
+    public void setFechaPaciente(Date fechaPaciente) {
+        this.fechaPaciente = fechaPaciente;
+    }
+
+    public String getResumenConsul() {
+        return resumenConsul;
+    }
+
+    public void setResumenConsul(String resumenConsul) {
+        this.resumenConsul = resumenConsul;
+    }
+
+    /**
+     * Get the value of string
+     *
+     * @return the value of string
+     */
+    public Paciente getPacienteConsulta() {
+        return pacienteConsulta;
+    }
+
+    /**
+     * Set the value of string
+     *
+     * @param string new value of string
+     */
+    public void setPacienteConsulta() {
+        this.pacienteConsulta= new Paciente(id, tipoId, nombrePaciente, fechaPaciente);
+    }
+
     
 }
